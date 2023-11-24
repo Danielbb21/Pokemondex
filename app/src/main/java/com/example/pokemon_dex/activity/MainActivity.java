a1b492b20baaf5b780295351fdddb4809287e1dc
João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>
2023-11-24 17:05:20 -0300

    tela de escolher pokemon initial
    Co-Authored-By: Danielbb21 <50447725+Danielbb21@users.noreply.github.com>

--- app/src/main/java/com/example/pokemon_dex/activity/MainActivity.java
+++ app/src/main/java/com/example/pokemon_dex/activity/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.pokemon_dex.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pokemon_dex.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button BtNewGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BtNewGame = findViewById(R.id.BtNewGame);
+        BtNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), initials_pokemons.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pokemon_dex/activity/initials_pokemons.java
@@ -0,0 +1,18 @@
+package com.example.pokemon_dex.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pokemon_dex.R;
+
+public class initials_pokemons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initials_pokemons);
+    }
+
+
+}
